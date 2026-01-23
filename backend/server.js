1168c4f3043f659ba613a927acace48962968553
bue-tshirts <buetshirts@gmail.com>
2026-01-23 10:37:53 +0000

    backend done

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,32 @@
-import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import express from "express";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import productRoutes from "./routes/products.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:" + PORT);
+
+    connectDB();
+
 });
