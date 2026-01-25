19566e9a85caa302f8980092e81bdc9f82959e10
bue-tshirts <buetshirts@gmail.com>
2026-01-25 18:12:00 +0000

    Fix admin page

--- frontend/src/Pages/AdminPage.jsx
+++ frontend/src/Pages/AdminPage.jsx
@@ -1,6 +1,6 @@
+import { motion } from "framer-motion";
 import { BarChart, PlusCircle, ShoppingBasket } from "lucide-react";
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
 
 import AnalyticsTab from "../components/AnalyticsTab";
 import CreateProductForm from "../components/CreateProductForm";
@@ -38,11 +38,10 @@ const AdminPage = () => {
 						<button
 							key={tab.id}
 							onClick={() => setActiveTab(tab.id)}
-							className={`flex items-center px-4 py-2 mx-2 rounded-md transition-colors duration-200 ${
-								activeTab === tab.id
+							className={`flex items-center px-4 py-2 mx-2 rounded-md transition-colors duration-200 ${activeTab === tab.id
 									? "bg-orange-600 text-white"
 									: "bg-gray-700 text-gray-600 hover:bg-gray-600"
-							}`}
+								}`}
 						>
 							<tab.icon className='mr-2 h-5 w-5' />
 							{tab.label}
